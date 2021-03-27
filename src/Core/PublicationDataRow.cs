b65a252447a1071e22462e582587507f599cfc7d
Amael BERTEAU <amael.berteau@tech-easy.fr>
2021-03-28 00:47:26 +0100

    [Core] Adding PublicationDataRow

---
+++ src/Core/PublicationDataRow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CafeynCatalogTools.Core
+{
+    public class PublicationDataRow
+    {
+        public Int32 StoreId { get; set; }
+
+        public String StoreName { get; set; }
+
+        public Int32 CategoryId { get; set; }
+
+        public String CategoryName { get; set; }
+
+        public Int32 PublicationId { get; set; }
+
+        public String PublicationName { get; set; }
+    }
+}
---
+++ src/Core/PublicationDataRowHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CafeynCatalogTools.Core.CafeynAPI;
+using CafeynCatalogTools.Core.CafeynAPI.Data;
+
+namespace CafeynCatalogTools.Core
+{
+    public class PublicationDataRowHelper
+    {
+        private static IEnumerable<PublicationDataRow> ToPublicationDataRows(IEnumerable<Store> stores)
+        {
+            IList<PublicationDataRow> rows = new List<PublicationDataRow>();
+            foreach (Store store in stores)
+            {
+                foreach (Category category in store.Categories)
+                {
+                    foreach (CategoryItem categoryItem in category.Items)
+                    {
+                        rows.Add(new PublicationDataRow()
+                        {
+                            StoreId = store.StoreId,
+                            StoreName = store.Name,
+                            CategoryId = category.Id,
+                            CategoryName = category.Name,
+                            PublicationId = categoryItem.PublicationId,
+                            PublicationName = categoryItem.Title
+                        });
+                    }
+                }
+            }
+
+            return rows;
+        }
+
+        public static async Task<IEnumerable<PublicationDataRow>> GetPublicationsAsync()
+        {
+            IEnumerable<Store> stores = await ApiClientHelper.GetStoresAsync();
+            IEnumerable<PublicationDataRow> publicationDataRows = ToPublicationDataRows(stores);
+            return publicationDataRows;
+        }
+    }
+}
