1af3cf3f67dd5dedf5a0e21d7e7b28e68e0cdb05
Amael BERTEAU <amael.berteau@tech-easy.fr>
2021-03-27 22:14:42 +0100

    Adding solution "CafeynCatalogTools" (+ project "ConsoleApp")

---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CafeynCatalogTools.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
