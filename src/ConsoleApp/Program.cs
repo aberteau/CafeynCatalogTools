861f937aed9957e39ea173e6da601c5c0b22d5b5
Amael BERTEAU <amael.berteau@tech-easy.fr>
2021-03-28 01:55:50 +0100

    [ConsoleApp] Adding fetch/serialization in Program

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using CafeynCatalogTools.Core;
 
 namespace CafeynCatalogTools.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private const string OutputPath = @""; // Chemin d'export à renseigner
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IEnumerable<PublicationDataRow> publicationDataRows = await PublicationDataRowHelper.GetPublicationsAsync();
+            string path = GetXmlFilePath();
+            PublicationDataRowHelper.Serialize(publicationDataRows, path);
+        }
+
+        private static string GetXmlFilePath()
+        {
+            string chrono = DateTime.Now.ToString("yyMMddHHmm");
+            string filename = $"cafeyn-bouygues-publications-{chrono}.xml";
+            string path = Path.Combine(OutputPath, filename);
+            return path;
         }
     }
 }
